04eba6078151468a1fe02a71ba703123174c0de0
Kurenai <kurenai233@yahoo.com>
2025-07-02 17:42:39 +0800

    chore: add db config

--- core/src/main/kotlin/moe/kurenai/bot/repository/DBHolder.kt
+++ core/src/main/kotlin/moe/kurenai/bot/repository/DBHolder.kt
@@ -1,10 +1,24 @@
 package moe.kurenai.bot.repository
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.babyfish.jimmer.sql.dialect.SQLiteDialect
+import org.babyfish.jimmer.sql.event.TriggerType
+import org.babyfish.jimmer.sql.kt.newKSqlClient
+import org.babyfish.jimmer.sql.runtime.ConnectionManager
+
 object DBHolder {
-    val sqlClient = {
-        HikariConfig().apply {
-            jdbcUrl = "jdbc:sqlite:im-sync-bot.db"
+    val sqlClient = lazy {
+        val config = HikariConfig().apply {
+            jdbcUrl = "jdbc:sqlite:bangumi-bot.db"
             driverClassName = "org.sqlite.JDBC"
+            isAutoCommit = true
+            maximumPoolSize = 1
+        }
+        newKSqlClient {
+            setTriggerType(TriggerType.BINLOG_ONLY)
+            setDialect(SQLiteDialect())
+            setConnectionManager(ConnectionManager.simpleConnectionManager(HikariDataSource(config)))
         }
     }
 }
