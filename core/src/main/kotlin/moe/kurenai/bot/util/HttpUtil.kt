fa02e9d8a6b6bb76e474ad9112609386a0ed4e26
Kurenai <kurenai233@yahoo.com>
2026-04-09 10:41:45 +0800

    chore(core): optimize ua fetch

--- core/src/main/kotlin/moe/kurenai/bot/util/HttpUtil.kt
+++ core/src/main/kotlin/moe/kurenai/bot/util/HttpUtil.kt
@@ -1,17 +1,26 @@
 package moe.kurenai.bot.util
 
 import io.ktor.client.*
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.api.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import jdk.dynalink.StandardOperation
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableSharedFlow
 import moe.kurenai.common.util.getLogger
 import moe.kurenai.common.util.json
 import org.jsoup.Jsoup
+import java.nio.file.StandardOpenOption
 import java.util.concurrent.atomic.AtomicReference
+import kotlin.io.path.Path
+import kotlin.io.path.exists
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
+import kotlin.io.path.writer
 import kotlin.time.Duration.Companion.days
 
 /**
@@ -22,45 +31,67 @@ import kotlin.time.Duration.Companion.days
 object HttpUtil {
 
     private val log = getLogger()
+    private val uaPath = Path("config/UA.txt")
     private val client = HttpClient {
         install(ContentNegotiation) {
             json(json)
         }
+        install(HttpTimeout) {
+            requestTimeoutMillis = 30_000
+        }
     }
     private val scope = CoroutineScope(Dispatchers.IO + CoroutineName("HttpUtil") + SupervisorJob())
-    private val uaRef = AtomicReference("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0")
+    private val uaUpdateFlow = MutableSharedFlow<String>()
 
-    val USER_AGENT
-        get() = uaRef.get()
+    @Volatile
+    var UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0"
+        private set
 
     val DYNAMIC_USER_AGENT = createClientPlugin("DynamicUserAgent") {
         onRequest { req, _ ->
-            req.header(HttpHeaders.UserAgent, uaRef.get())
+            req.header(HttpHeaders.UserAgent, UA)
         }
     }
 
     init {
         scope.launch {
+            uaUpdateFlow.collect {
+                runCatching {
+                    if (UA != it) {
+                        UA = it
+                        uaPath.writeText(it, Charsets.UTF_8, StandardOpenOption.APPEND)
+                        log.info("Update UA: {}", it)
+                    }
+                }.onFailure { ex ->
+                    log.error("Update UA error", ex)
+                }
+            }
+        }
+        scope.launch {
+            runCatching {
+                if (uaPath.exists()) {
+                    uaUpdateFlow.emit(uaPath.readText())
+                }
+            }.onFailure {
+                log.error("Read UA file error", it)
+            }
+
             while (true) {
                 runCatching {
-                    uaRef.set(getLatestUA())
-                    log.info("Update UA: {}", uaRef.get())
+                    val ua = getLatestUA()
+                    uaUpdateFlow.emit(ua)
                 }.onFailure {
-                    log.error("Update ua error", it)
+                    log.error("Fetch latest UA error", it)
                 }
                 delay(1.days)
             }
-        }
-        runCatching {
-            uaRef.set(runBlocking { getLatestUA() })
-        }.onFailure {
-            log.error("Update ua error", it)
         }
     }
 
     private suspend fun getLatestUA(): String {
         return json.decodeFromString<List<String>>(
-            client.get("https://jnrbsn.github.io/user-agents/user-agents.json").bodyAsText()
+            client.get("https://jnrbsn.github.io/user-agents/user-agents.json")
+                .bodyAsText()
         ).last { it.contains("Windows NT") && it.contains("Chrome") }
     }
 
